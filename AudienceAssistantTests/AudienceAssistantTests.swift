1e3103b7d4901ba7a84ad523a737308e48bf8ef6
Jake Levy <jake.r.levy@gmail.com>
2025-07-13 02:22:32 -0400

    initial commit

---
+++ AudienceAssistantTests/AudienceAssistantTests.swift
@@ -0,0 +1,17 @@
+//
+//  AudienceAssistantTests.swift
+//  AudienceAssistantTests
+//
+//  Created by Jacob Levy on 7/13/25.
+//
+
+import Testing
+@testable import AudienceAssistant
+
+struct AudienceAssistantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
